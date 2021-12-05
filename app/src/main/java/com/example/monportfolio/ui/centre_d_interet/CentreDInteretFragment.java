b0f2ef89b3d4538fb24342ad65a87058a3ea6898
MadiKABA <madikaba2004@gmail.com>
2021-12-05 23:33:54 +0000

    stylisation de mon portfolio(toutes les sections).

--- app/src/main/java/com/example/monportfolio/ui/centre_d_interet/CentreDInteretFragment.java
+++ app/src/main/java/com/example/monportfolio/ui/centre_d_interet/CentreDInteretFragment.java
@@ -1,6 +1,34 @@
 package com.example.monportfolio.ui.centre_d_interet;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.monportfolio.databinding.FragmentCentreDInteretBinding;
 
 public class CentreDInteretFragment extends Fragment {
+    private CentreDInteretViewModel centreDInteretViewModel;
+    private FragmentCentreDInteretBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        centreDInteretViewModel =
+                new ViewModelProvider(this).get(CentreDInteretViewModel.class);
+
+        binding = FragmentCentreDInteretBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
--- app/src/main/java/com/example/monportfolio/ui/centre_d_interet/CentreDInteretViewModel.java
+++ app/src/main/java/com/example/monportfolio/ui/centre_d_interet/CentreDInteretViewModel.java
@@ -1,6 +1,7 @@
 package com.example.monportfolio.ui.centre_d_interet;
-
 import androidx.lifecycle.ViewModel;
 
+
 public class CentreDInteretViewModel extends ViewModel {
+
 }
